1f22d56ed1d7c27c77fa70f91e23a68d0ce2e13d
egrosner <egrosner@gmail.com>
2016-12-18 11:06:03 -0500

    db now uses a url

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
 const express = require('express');
 const pug = require('pug');
+const Sequelize = require('sequelize');
 
 const app = express();
+
+var sequelize = new Sequelize(process.env.JAWDB_URL);
+
+var BlogPost = sequelize.define('blogpost', {
+  id: {
+      type: Sequelize.UUID,
+      primaryKey: true,
+      defaultValue: Sequelize.UUIDV4
+  },
+  title: Sequelize.STRING
+});
+
+sequelize.sync();
+
+/*
+BlogPost.upsert({
+    title: 'second post!'
+});
+*/
 
 //server up the semantic css
 app.use('/semantic/dist', express.static('semantic/dist'));
@@ -11,12 +31,16 @@ app.use(express.static('images'));
 app.set('view engine', 'pug');
 
 //about page
-app.get('/about', (req, res) => res.render('about') );
+app.get('/about', (req, res) => {
+    res.render(req.route.path.substr(1, req.route.path.length));
+});
 
 //home page render
-app.get('/', (req, res) => res.render('index') );
-
-
+app.get('/', (req, res) => {
+    Model.findAll().then(posts => {
+        res.render('index', posts);
+    });
+});
 
 //server
 app.listen(8080);
